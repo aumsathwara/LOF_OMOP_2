f2915704053f293d0faca03b185fcaa21c286d60
Vishnu Priyan S S <vishnu32510@gmail.com>
2025-04-23 19:49:59 -0500

    ETL2

---
+++ omop_mapper.py
@@ -0,0 +1,9 @@
+class DataMapper:
+    """ 
+        Class to encapsulate mapping functionality for ETL
+    """
+    
+    def map_x_to_y(self, x: any):
+        """ Maps x to y """
+        pass
+    
