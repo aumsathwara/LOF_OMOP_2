522efa92f8010c85ea03d0b098c3dd3d17db7c8c
aumsathwara <aumsathwara2811@gmail.com>
2025-05-02 12:21:33 -0500

    add readme, mod etland MCP

--- omop_mapper.py
+++
@@ -1,9 +0,0 @@
-class DataMapper:
-    """ 
-        Class to encapsulate mapping functionality for ETL
-    """
-    
-    def map_x_to_y(self, x: any):
-        """ Maps x to y """
-        pass
-    
